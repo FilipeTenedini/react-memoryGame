c5931606a808e0b470271f94a89a3ad87749a430
filipetenedini <tenedinifilipe@gmail.com>
2023-02-04 00:48:40 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import * as C from './App.styles'; 
+import logoImage from './assets/devmemory_logo.png';
+
+const App = () => {
+
+    return (
+        <C.Container>
+            <C.Info>
+                <C.LogoLink href="">
+                    <img src={logoImage} alt="" />
+                </C.LogoLink>
+
+                <C.InfoArea>
+                    ...
+                </C.InfoArea>
+                
+                <button>Reiniciar</button>
+            
+            </C.Info>
+            
+            <C.GridArea>
+            
+            </C.GridArea>
+
+        </C.Container>
+    );
+}
+
+export default App;
