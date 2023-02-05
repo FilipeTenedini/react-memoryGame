5be374fede029569751855cdd749ad322b8ee05b
filipetenedini <tenedinifilipe@gmail.com>
2023-02-04 23:45:42 -0300

    criando grid item

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
 import * as C from './App.styles'; 
-import logoImage from './assets/devmemory_logo.png';
-import btnRestartIcon from './svgs/restart.svg';
+import logoImage from './assets/images/devmemory_logo.png';
+import btnRestartIcon from './assets/svgs/restart.svg';
 import { InfoItem } from './components/InfoItem';
 import { Button } from './components/button';
+import { GridItemType } from './types/GridItemType';
+
 const App = () => {
+    const [playing, setPlaying] = useState<boolean>(false);
+    const [timeElapsed, setTimeElapsed] = useState<number>(0);
+    const [moveCount, setMoveCount] = useState<number>(0);
+    const [shownCount, setShownCount] = useState<number>(0);
+    const [gridItem, setGridItems] = useState<GridItemType[]>([]);
+
+
+    useEffect(() => resetAndCreateGrid(), []);
 
     const resetAndCreateGrid = () => {
         
     }
+
     return (
         <C.Container>
             <C.Info>
@@ -25,7 +37,9 @@ const App = () => {
             </C.Info>
             
             <C.GridArea>
-                ...
+                <C.Grid>
+
+                </C.Grid>
             </C.GridArea>
 
         </C.Container>
